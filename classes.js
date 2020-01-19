121db191f56adeaed33d15ef10f2a5ca653802a3
Tucker Willenborg <admin@rivers.run>
2020-01-19 16:38:05 -0500

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,5 +1,5 @@
 ;(async function() {
-  let request = await fetch("testData.json");
+  let request = await fetch("sampleData.json");
   let classes = await request.json()
   
   console.log(classes)
