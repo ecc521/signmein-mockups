062a993f05d19284458232af78d703d527585acd
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-01-19 17:29:09 -0500

    Expand for Class Details

--- classes.js
+++ classes.js
@@ -40,9 +40,33 @@
 
   let classes = data.classes
   classes.forEach((classInfo) => {
-      //Add a row for the class. 
-      table.appendChild(createClassRow(classInfo))
+      //Add a row for the class.
+      let row =  createClassRow(classInfo)
+      table.appendChild(row)
+
+      let expanded = false
+      row.addEventListener("click", function() {
+          if (expanded) {
+              row.nextElementSibling.remove()
+          }
+          else {
+              let classView = createClassView(classInfo)
+              row.parentNode.insertBefore(classView, row.nextElementSibling)
+          }
+          expanded = !expanded
+      })
   })
+
+  function createClassView(classInfo) {
+      let div = document.createElement("div")
+      div.className = "classInfo"
+
+      let name = document.createElement("h2")
+      name.innerHTML = classInfo.name
+      div.appendChild(name)
+
+      return div
+  }
 
 
 
