de51e56e01f249e3b656b7c8626831da2c44df8a
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-01-19 17:07:46 -0500

    More Work on classes.html

--- classes.js
+++ classes.js
@@ -5,5 +5,41 @@
   console.log(data)
   let header = document.querySelector("h1")
   header.innerText = data.name + "'s Classes"
-  
+
+  let table = document.getElementById("classes")
+
+
+  function createClassRow(classInfo) {
+      let tableRow = document.createElement("tr")
+
+      function addTh(text) {
+          let th = document.createElement("th")
+          th.innerText = text
+          tableRow.appendChild(th)
+      }
+
+      addTh(classInfo.name)
+      addTh(classInfo.notes)
+      if (typeof classInfo.students === "string") {
+          //For creating the header
+          addTh(classInfo.students)
+      }
+      else {
+          addTh(classInfo.students.length)
+      }
+      return tableRow
+  }
+
+
+  table.appendChild(createClassRow({
+      name: "Class Name",
+      notes: "Notes",
+      students: "Number of Students"
+  }))
+  let classes = data.classes
+  classes.forEach((classInfo) => {
+      table.appendChild(createClassRow(classInfo))
+  })
+
+
 }())
